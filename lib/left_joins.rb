e90e7c337497dddc40d943e92ce01a39bc55fdfa
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-12-23 18:00:35 +0800

    Add: join dependency

--- lib/left_joins.rb
+++ lib/left_joins.rb
@@ -13,6 +13,31 @@ class ActiveRecord::Relation
   alias_method :left_outer_joins, :left_joins if not method_defined?(:left_outer_joins)
 end
 
+module ActiveRecord
+  module Associations
+    class JoinDependency
+      if not method_defined?(:make_outer_joins)
+        def join_constraints(outer_joins, join_type = nil)
+          @_left_joins_join_type = join_type if join_type
+          result = join_constraints_without_join_type(outer_joins)
+          @_left_joins_join_type = nil
+          return result
+        end
+
+        private
+
+        def make_inner_joins(parent, child)
+          tables    = table_aliases_for(parent, child)
+          join_type = @_left_joins_join_type || Arel::Nodes::InnerJoin
+          info      = make_constraints parent, child, tables, join_type
+
+          [info] + child.children.flat_map { |c| make_inner_joins(child, c) }
+        end
+      end
+    end
+  end
+end
+
 class ActiveRecord::Base
   def self.left_joins(*args, &block)
     self.where('').left_joins(*args, &block)
