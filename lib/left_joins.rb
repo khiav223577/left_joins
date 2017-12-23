f39c551aa52e6d5c4f7580af7809d01b4038041e
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-12-23 17:47:07 +0800

    Add: left_joins method for relation

--- lib/left_joins.rb
+++ lib/left_joins.rb
@@ -1,3 +1,24 @@
 require "left_joins/version"
 require 'active_record'
 
+class ActiveRecord::Relation
+  IS_RAILS3_FLAG = Gem::Version.new(ActiveRecord::VERSION::STRING) < Gem::Version.new('4.0.0')
+  IS_RAILS5_FLAG = Gem::Version.new(ActiveRecord::VERSION::STRING) >= Gem::Version.new('5.0.0')
+  if not method_defined?(:left_joins)
+    def left_joins(args)
+      6
+    end
+  end
+
+  alias_method :left_outer_joins, :left_joins if not method_defined?(:left_outer_joins)
+end
+
+class ActiveRecord::Base
+  def self.left_joins(*args, &block)
+    self.where('').left_joins(*args, &block)
+  end
+
+  def self.left_outer_joins(*args, &block)
+    self.where('').left_outer_joins(*args, &block)
+  end
+end
