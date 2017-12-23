8aa54d5b4e72b905f279e7609b53d1cd047b2bf5
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-12-23 17:43:07 +0800

    test aliasing

--- test/left_joins_test.rb
+++ test/left_joins_test.rb
@@ -9,4 +9,8 @@ class LeftJoinsTest < Minitest::Test
     assert_equal 6, User.joins(:posts).count
     assert_equal 7, User.left_joins(:posts).count
   end
+
+  def test_left_outer_joins_alias
+    assert_equal User.left_outer_joins(:posts).count, User.left_joins(:posts).count
+  end
 end
