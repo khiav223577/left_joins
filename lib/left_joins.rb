2f9902e745b917c3c6277c3fc07efd4c6a8e572b
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-12-25 11:34:10 +0800

    Fix count which would sometimes force a DISTINCT
    https://github.com/rails/rails/pull/26121

--- lib/left_joins.rb
+++ lib/left_joins.rb
@@ -71,6 +71,28 @@ module ActiveRecord::QueryMethods
         end
       end
     end
+
+    module ActiveRecord::Calculations
+      def perform_calculation(operation, column_name, _options = {})
+        operation = operation.to_s.downcase
+
+        # If #count is used with #distinct (i.e. `relation.distinct.count`) it is
+        # considered distinct.
+        distinct = self.distinct_value
+
+        if operation == "count"
+          column_name ||= select_for_count
+          column_name = primary_key if column_name == :all && distinct
+          distinct = nil if column_name =~ /\s*DISTINCT[\s(]+/i
+        end
+
+        if group_values.any?
+          execute_grouped_calculation(operation, column_name, distinct)
+        else
+          execute_simple_calculation(operation, column_name, distinct)
+        end
+      end
+    end
   end
 end
 
--- test/left_joins_test.rb
+++ test/left_joins_test.rb
@@ -7,7 +7,7 @@ class LeftJoinsTest < Minitest::Test
 
   def test_left_joins
     assert_equal 6, User.joins(:posts).count
-    assert_equal 7, User.left_joins(:posts).to_a.size # FIXME
+    assert_equal 7, User.left_joins(:posts).count
   end
 
   def test_left_outer_joins_alias
