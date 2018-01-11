24cc2febc55a46e236cef8cf4d8b6decd1eb0105
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-01-12 03:06:29 +0800

    test left joins on association

--- test/left_joins_test.rb
+++ test/left_joins_test.rb
@@ -13,4 +13,9 @@ class LeftJoinsTest < Minitest::Test
   def test_left_outer_joins_alias
     assert_equal User.left_outer_joins(:posts).count, User.left_joins(:posts).count
   end
+
+  def test_left_joins_on_has_many_association
+    assert_equal 1, User.find_by(name: 'John1').posts.joins(:post_comments).distinct.size
+    assert_equal 3, User.find_by(name: 'John1').posts.left_joins(:post_comments).distinct.size
+  end
 end
