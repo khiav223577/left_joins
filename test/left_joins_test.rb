d390f101be7cab4659aaa395bf9c0d6a89069734
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2019-08-12 17:22:59 +0800

    test left joins with distinct

--- test/left_joins_test.rb
+++ test/left_joins_test.rb
@@ -15,6 +15,14 @@ class LeftJoinsTest < Minitest::Test
     assert_equal User.left_outer_joins(:posts).count, User.left_joins(:posts).count
   end
 
+  def test_left_joins_with_distinct
+    assert_equal 3, User.joins(:posts).distinct.count
+    assert_equal 3, User.distinct.joins(:posts).count
+
+    assert_equal 4, User.left_joins(:posts).distinct.count
+    assert_equal 4, User.distinct.left_joins(:posts).count
+  end
+
   def test_left_joins_on_has_many_association
     assert_equal [
       "John1's post3",
